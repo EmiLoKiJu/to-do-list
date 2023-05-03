0b7b29567d8b2ca5856b68371bedfb7fba4f8a54
unknown <gabrielrozas1996@gmail.com>
2023-05-03 09:20:30 -0800

    Add ul element and css style

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import _ from 'lodash';
 import './style.css';
-function component() {
-    const element = document.createElement('div');
+// function component() {
+//   const element = document.createElement('h1');
   
-      // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hi', 'wtf'], ' ');
+//       // Lodash, now imported by this script
+//   element.innerHTML = _.join([''], ' ');
   
-    return element;
-  }
-  
-  document.body.appendChild(component());
+//   return element;
+// }
+// document.body.appendChild(component());
+
+let arraylist = [];
+
+
+

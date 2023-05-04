ead27e8f09c51e80fd0286d0ab6c1ccd537b2b79
unknown <gabrielrozas1996@gmail.com>
2023-05-03 22:12:38 -0800

    Add functionalities for editing

--- src/modules/iteratearray.js
+++ src/modules/iteratearray.js
@@ -11,10 +11,28 @@ const iteratearray = (arr) => {
       <input type="checkbox" class="iscompleted">
       <div class="listelement">${arr[i].description}</div>
     </div>
-    <div class="3dotsicon"></div>
+    <div class="3dotsicon"><img class="icon" src="./img/3dotsicon.png" alt="grabme"></div>
+    <div class="deletebutton dnone"><img class="icon" src="./img/deleteicon.png" alt="delete"></div>
     `;
     element.innerHTML = strelement;
     listelementcontainer.appendChild(element);
+    const textdiv = document.querySelector('.listelement');
+    textdiv.classList.add('yellowbg');
+    textdiv.addEventListener('dbclick', () => {
+      const newinput = document.createElement('input');
+      newinput.type = 'text';
+      newinput.classList.add('yellowbg');
+      newinput.value = textdiv.innerText;
+      textdiv.parentNode.replaceChild(newinput, textdiv);
+      newinput.focus();
+      newinput.select();
+      newinput.addEventListener('blur', () => {
+        const newtextdiv = document.createElement('div');
+        newtextdiv.classList.add('listelement');
+        newtextdiv.innerText = newinput.value;
+        newinput.parentNode.replaceChild(newtextdiv, newinput);
+      });
+    });
   }
 };
 

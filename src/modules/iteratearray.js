7ff26e119a004373ba234c13d0d4bd35d326791c
unknown <gabrielrozas1996@gmail.com>
2023-05-04 12:10:27 -0800

    Fix linter

--- src/modules/iteratearray.js
+++ src/modules/iteratearray.js
@@ -1,6 +1,35 @@
 const listelementcontainer = document.querySelector('.listelementcontainer');
 let editelement;
-let iteratearray;
+
+const iteratearray = (arr) => {
+  listelementcontainer.innerHTML = ' ';
+  for (let i = 0; i < arr.length; i += 1) {
+    const element = document.createElement('div');
+    element.classList.add('dflex');
+    element.classList.add('spacebetween');
+    const listelement = document.createElement('div');
+    listelement.classList.add('listelement');
+    listelement.innerText = arr[i].description;
+    const strelement = `
+    <div class="dflex">
+      <input type="checkbox" class="iscompleted">
+      <div class="specificcontainer">
+      </div>
+    </div>
+    <div>
+      <img class="icon threedotsicon" src="./img/threedotsicon.png" alt="grabme">
+      <img class="icon deleteicon dnone" src="./img/deleteicon.png" alt="delete">
+    </div>
+    `;
+    element.innerHTML = strelement;
+    listelementcontainer.appendChild(element);
+    const specificcontainer = element.querySelector('.specificcontainer');
+    specificcontainer.appendChild(listelement);
+    listelement.addEventListener('dblclick', () => {
+      editelement(listelement, element, arr, i);
+    });
+  }
+};
 
 const switchelement = (inputelement, elementcontainer, arr, i) => {
   const newlistelement = document.createElement('div');
@@ -39,36 +68,6 @@ editelement = (listelement, elementcontainer, arr, i) => {
       switchelement(newinput, elementcontainer);
     }, 100);
   });
-};
-
-iteratearray = (arr) => {
-  listelementcontainer.innerHTML = ' ';
-  for (let i = 0; i < arr.length; i += 1) {
-    const element = document.createElement('div');
-    element.classList.add('dflex');
-    element.classList.add('spacebetween');
-    const listelement = document.createElement('div');
-    listelement.classList.add('listelement');
-    listelement.innerText = arr[i].description;
-    const strelement = `
-    <div class="dflex">
-      <input type="checkbox" class="iscompleted">
-      <div class="specificcontainer">
-      </div>
-    </div>
-    <div>
-      <img class="icon threedotsicon" src="./img/threedotsicon.png" alt="grabme">
-      <img class="icon deleteicon dnone" src="./img/deleteicon.png" alt="delete">
-    </div>
-    `;
-    element.innerHTML = strelement;
-    listelementcontainer.appendChild(element);
-    const specificcontainer = element.querySelector('.specificcontainer');
-    specificcontainer.appendChild(listelement);
-    listelement.addEventListener('dblclick', () => {
-      editelement(listelement, element, arr, i);
-    });
-  }
 };
 
 export default iteratearray;
